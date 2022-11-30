115d17f75329368951b21ebb3854222ed6aeb87d
Szymon <shateq21@gmail.com>
2022-11-30 19:40:46 +0100

    Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,32 @@
 plugins {
-    id("java")
+    java
+    kotlin("jvm") version "1.7.10"
+
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("application")
 }
 
-version = "0.0.1"
-mainClassName = project.maven_group + ".Main"
-group = project.maven_group
+version = "2.0.1"
+group = "shateq.java"
 base.archivesName.set("${project.name}-jdk17-$version")
 description = "Discord bot attempt"
 
 repositories.mavenCentral()
 
 dependencies {
-    // JDA
-    implementation("net.dv8tion:JDA:5.0.0-beta.1") {
-        exclude module: "opus-java"
+    implementation("net.dv8tion:JDA:5.0.0-beta.1") { //JDA
+        exclude("opus-java")
     }
     implementation("com.sedmelluq:lavaplayer:1.3.77")
     implementation("org.jetbrains:annotations:23.0.0")
     // jedis
-
     // Utils
     implementation("io.github.cdimascio:dotenv-java:2.3.1")
     implementation("org.slf4j:slf4j-simple:2.0.5")
-    //implementation("ch.qos.logback:logback-classic:1.4.5")
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+application.mainClass.set("shateq.java.moonlight.MoonlightBot")
 
 tasks {
     processResources {
@@ -35,16 +34,16 @@ tasks {
         inputs.property("version", project.version)
     }
     compileJava {
-        it.options.encoding = "UTF-8"
-        it.options.release.set(17)
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+    shadowJar {
+        minimize()
     }
     jar {
-        manifest {
-            attributes(
-                    "Main-Class": mainClassName,
-                    "Implementation-Title": project.name,
-                    "Implementation-Version": project.version
-            )
-        }
+        manifest.attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
     }
 }
